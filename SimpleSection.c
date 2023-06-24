dfe89991088df414960ea2b8763f5303d861f63e
RicardoJiang <2868405029@qq.com>
2023-06-24 15:59:07 +0800

    ADD: SimpleSection.c

---
+++ SimpleSection.c
@@ -0,0 +1,40 @@
+/* 
+
+ * SimpleSection.c
+
+ * 
+
+ * Linux:
+
+ *   gcc -c SimpleSection.c
+
+ *
+
+ * Windows:
+
+ *   cl SimpleSection.c /c /Za
+
+ */
+
+int printf(const char *format, ...);
+
+int global_init_var = 84;
+int global_uninit_var;
+
+void fun1(int i)
+{
+    printf("%d\n", i);
+}
+
+int main(void)
+{
+    static int static_var = 85;
+    static int static_var2;
+
+    int a = 1;
+    int b;
+    fun1(static_var + static_var2 + a + b);
+    return a;
+}
+
+
